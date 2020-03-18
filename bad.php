6acd8edfbadc7a666ee93ec8450d1d052620ee5d
Will Baizer <will@thebaizers.com>
2020-03-18 16:30:14 -0500

    Update bad.php

--- bad.php
+++ bad.php
@@ -4,4 +4,40 @@ $aiousfhd = 1; $aiousfhd = 1;
 
 $aiousfhd = 1;
 
+$asddf = 1; $dfasdf1;
+$asddf = 1; $dfasdf2;
+$asddf = 1; $dfasdf3;
+$asddf = 1; $dfasdf4;
+$asddf = 1; $dfasdf5;
+$asddf = 1; $dfasdf6;
+$asddf = 1; $dfasdf7;
+$asddf = 1; $dfasdf8;
+$asddf = 1; $dfasdf9;
+$asddf = 1; $dfasdf10;
+$asddf = 1; $dfasdf11;
+$asddf = 1; $dfasdf12;
+$asddf = 1; $dfasdf13;
+$asddf = 1; $dfasdf14;
+$asddf = 1; $dfasdf15;
+$asddf = 1; $dfasdf16;
+$asddf = 1; $dfasdf17;
+$asddf = 1; $dfasdf18;
+$asddf = 1; $dfasdf19;
+$asddf = 1; $dfasdf20;
+$asddf = 1; $dfasdf21;
+$asddf = 1; $dfasdf22;
+$asddf = 1; $dfasdf23;
+$asddf = 1; $dfasdf24;
+$asddf = 1; $dfasdf25;
+$asddf = 1; $dfasdf26;
+$asddf = 1; $dfasdf27;
+$asddf = 1; $dfasdf28;
+$asddf = 1; $dfasdf29;
+$asddf = 1; $dfasdf30;
+$dfasdf28 = $dfasdf30;
+
+function hello(){ $dfasdf30;
+
+$dfasdf30;}
+
 ?>
