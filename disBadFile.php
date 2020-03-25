b234a8d5efaa4ec4a8d687f2b82580d34bcaeede
Will Baizer <will@thebaizers.com>
2020-03-25 11:00:29 -0500

    Create disBadFile.php

---
+++ disBadFile.php
@@ -0,0 +1,45 @@
+<?php $hello = 'yo';
+
+asdf;
+
+$lkas = $jdhfh;
+
+$yo = $hfuipoi;
+
+$wow = $yo; $turnt = 98273409;
+$blah = $blah;
+
+protected function asdf($dude) : Hello {"adsjfas";
+"asfadf";
+  $yo;
+  $yo = 987;
+  $stuff; $stuff; $stuff;
+  $stuff; $stuff; $stuff;
+  $stuff; $stuff; $stuff;
+  $stuff; $stuff; $stuff;
+  $stuff; $stuff; $stuff;
+  $stuff; $stuff; $stuff;
+  $stuff; $stuff; $stuff;
+  $stuff; $stuff; $stuff;
+  $stuff; $stuff = $stuff ; $$stuff = $stuff ;
+  $stuff; $stuff = $stuff ; $$stuff = $stuff ;
+  $stuff; $stuff = $stuff ; $$stuff = $stuff ;
+  $stuff; $stuff = $stuff ; $$stuff = $stuff ;
+  $stuff; $stuff = $stuff ; $$stuff = $stuff ;
+  $stuff; $stuff = $stuff ; $$stuff = $stuff ;
+  $stuff; $stuff = $stuff ; $$stuff = $stuff ;
+  $stuff; $stuff = $stuff ; $$stuff = $stuff ;
+  $stuff; $stuff = $stuff ; $$stuff = $stuff ;
+  $stuff; $stuff = $stuff ; $$stuff = $stuff ;
+  return;
+  $stuff; $stuff = $stuff ; $$stuff = $stuff ;
+  $stuff; $stuff = $stuff ; $$stuff = $stuff ;
+  $stuff; $stuff = $stuff ; $$stuff = $stuff ;
+  $stuff; $stuff = $stuff ; $$stuff = $stuff ;
+  return $512 = 'asdf';
+}
+
+while (true) {
+  $asdfasdf = 1/0;}
+  
+echo $987;?>
