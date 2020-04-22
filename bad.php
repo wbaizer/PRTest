686b33d981e50a4a812d28b4438471b086fe99b7
Will Baizer <will@thebaizers.com>
2020-04-22 15:02:02 -0500

    Update bad.php

--- bad.php
+++ bad.php
@@ -5,6 +5,26 @@ $aiousfhd = 1;
 
 $hello = 1;
 
+$asddf = 1; $dfasdf2;
+$asddf = 1; $dfasdf3;
+$asddf = 1; $dfasdf4;
+$asddf = 1; $dfasdf5;
+$asddf = 1; $dfasdf6;
+$asddf = 1; $dfasdf7;
+$asddf = 1; $dfasdf8;
+$asddf = 1; $dfasdf9;
+$asddf = 1; $dfasdf10;
+$asddf = 1; $dfasdf11;
+$asddf = 1; $dfasdf12;
+$asddf = 1; $dfasdf13;
+$asddf = 1; $dfasdf14;
+$asddf = 1; $dfasdf15;
+$asddf = 1; $dfasdf16;
+$asddf = 1; $dfasdf17;
+$asddf = 1; $dfasdf18;
+$asddf = 1; $dfasdf19;
+$asddf = 1; $dfasdf20;
+$asddf = 1; $dfasdf21;
 
 function hello(){
   $dfasdf30 = $hello;
